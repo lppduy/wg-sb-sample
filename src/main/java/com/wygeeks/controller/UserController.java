4d76836f472faabf3a40b64d7b21ea27abe128e2
lppduy <lephamphuongduy@gmail.com>
2024-06-20 15:33:48 +0700

    feat: validate data in UserController

--- src/main/java/com/wygeeks/controller/UserController.java
+++ src/main/java/com/wygeeks/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.wygeeks.controller;
 
 import com.wygeeks.dto.request.UserRequestDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -10,30 +13,34 @@ import java.util.List;
 public class UserController {
 
 //    @PostMapping(value = "/", headers = "apiKey=v1.0")
-    @RequestMapping(value = "/", method = RequestMethod.POST, headers ="apiKey=v1.0")
-    public String addUser(@RequestBody UserRequestDTO user) {
+//    @RequestMapping(value = "/", method = RequestMethod.POST, headers ="apiKey=v1.0")
+    @PostMapping("/")
+    public String addUser(@Valid @RequestBody UserRequestDTO user) {
         return "User added";
     }
 
     @PutMapping("/{userId}")
-    public String updateUser(@PathVariable int userId, @RequestBody UserRequestDTO user) {
+    public String updateUser(@PathVariable int userId, @Valid @RequestBody UserRequestDTO user) {
         System.out.println("Update user");
         return "User updated";
     }
 
     @PatchMapping("/{userId}")
-    public String updateStatus(@PathVariable boolean enable) {
+    public String updateStatus(@PathVariable @Min(1) int userId, @RequestParam @Min(1) int status) {
         return "User Status changed";
     }
 
     @DeleteMapping("/{userId}")
-    public String deleteUser(@PathVariable int userId){
+    public String deleteUser(@Min(1) @PathVariable int userId){
         return "User deleted";
     }
 
     @GetMapping("/{userId}")
     public UserRequestDTO getUser(@PathVariable int userId) {
-        return new UserRequestDTO("Duy", "Le", "lppduy@gmail.com", "0975427777");
+        return new UserRequestDTO("Duy", "Le", "lppduy@gmail.com", "0975427777",
+                new Date("07/27/1997"),
+                List.of("ADMIN", "USER")
+        );
     }
 
     @GetMapping("/list")
@@ -43,8 +50,14 @@ public class UserController {
             @RequestParam(defaultValue = "10") int pageSize
     ) {
         return List.of(
-                new UserRequestDTO("Duy", "Le", "lppd1@gmail.com", "0975427777"),
-                new UserRequestDTO("Duy", "Le", "lppd2@gmail.com", "0975427777")
+                new UserRequestDTO("Duy", "Le", "lppd1@gmail.com", "0975427777",
+                        new Date("07/27/1997"),
+                        List.of("ADMIN", "USER")
+                ),
+                new UserRequestDTO("Duy", "Le", "lppd2@gmail.com", "0975427777",
+                        new Date("07/27/1997"),
+                        List.of("ADMIN", "USER")
+                )
         );
     }
 }
--- src/main/java/com/wygeeks/dto/request/UserRequestDTO.java
+++ src/main/java/com/wygeeks/dto/request/UserRequestDTO.java
@@ -1,16 +1,38 @@
 package com.wygeeks.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.wygeeks.util.PhoneNumber;
+import jakarta.validation.constraints.*;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRequestDTO implements Serializable {
+    @NotBlank(message = "firstName must be not blank") // Khong cho phep gia tri blank
     private String firstName;
+
+    @NotNull(message = "lastName must be not null") // Khong cho phep gia tri null
     private String lastName;
+
+    @Email(message = "email invalid format") // Chi chap nhan nhung gia tri dung dinh dang email
     private String email;
+
+//    @Pattern(regexp = "^\\d{10}$", message = "phone invalid format")
+    @PhoneNumber
     private String phone;
+
+    @NotNull(message = "dateOfBirth must be not null")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "MM/dd/yyyy")
+    private Date dateOfBirth;
+
+    @NotEmpty(message = "permissions must be not empty") // Khong cho phep gia tri rong
+    private List<String> permissions;
 }
